5031e8e4218866f9e4706dba5a3f181001f2969b
Markus J Doetsch <git@the-enemy.org>
2017-12-26 15:31:39 +0200

    add debugging & error handling

--- app/bootstrap_web.php
+++ app/bootstrap_web.php
@@ -3,70 +3,62 @@
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Mvc\Application;
 
-error_reporting(E_ALL);
-
-$debug = new Phalcon\Debug();
-$debug->listen();
-
+define('DEBUG', getenv('DEBUG') === 'true' ? true : false);
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
 
-// try {
+/**
+ * Include Autoloader
+ */
+include APP_PATH . '/config/loader.php';
 
-    /**
-     * The FactoryDefault Dependency Injector automatically registers the services that
-     * provide a full stack framework. These default services can be overidden with custom ones.
-     */
-    $di = new FactoryDefault();
+/**
+* Error handling
+*/
+Phalcon\Error\Handler::register();
 
-    /**
-     * Include general services
-     */
-    require APP_PATH . '/config/services.php';
+if (DEBUG === true) {
+    $debug = new Phalcon\Debug();
+    $debug->listen();
+}
 
-    /**
-     * Include web environment specific services
-     */
-    require APP_PATH . '/config/services_web.php';
+/**
+ * The FactoryDefault Dependency Injector automatically registers the services that
+ * provide a full stack framework. These default services can be overidden with custom ones.
+ */
+$di = new FactoryDefault();
 
-    /**
-     * Get config service for use in inline setup below
-     */
-    $config = $di->getConfig();
+/**
+ * Include general services
+ */
+require APP_PATH . '/config/services.php';
 
-    // if ($config->application->debug) {
-    //     $debug = new Phalcon\Debug();
-    //     $debug->listen();
-    // }
+/**
+ * Include web environment specific services
+ */
+require APP_PATH . '/config/services_web.php';
 
-// } catch (\Exception $e) {
-//     echo $e->getMessage() . '<br>';
-//     echo '<pre>' . $e->getTraceAsString() . '</pre>';
-// }
+/**
+ * Get config service for use in inline setup below
+ */
+$config = $di->getConfig();
 
-    // throw new \Exception("aaa");
+/**
+ * Handle the request
+ */
+$application = new Application($di);
 
-    /**
-     * Include Autoloader
-     */
-    include APP_PATH . '/config/loader.php';
+/**
+ * Register application modules
+ */
+$application->registerModules([
+    'frontend' => ['className' => 'Invo\Modules\Frontend\Module'],
+]);
 
-    /**
-     * Handle the request
-     */
-    $application = new Application($di);
+/**
+ * Include routes
+ */
+require APP_PATH . '/config/routes.php';
 
-    /**
-     * Register application modules
-     */
-    $application->registerModules([
-        'frontend' => ['className' => 'Invo\Modules\Frontend\Module'],
-    ]);
-
-    /**
-     * Include routes
-     */
-    require APP_PATH . '/config/routes.php';
-
-    // echo str_replace(["\n","\r","\t"], '', $application->handle()->getContent());
-    echo $application->handle()->getContent();
+// echo str_replace(["\n","\r","\t"], '', $application->handle()->getContent());
+echo $application->handle()->getContent();
--- app/modules/frontend/controllers/IndexController.php
+++ app/modules/frontend/controllers/IndexController.php
@@ -7,7 +7,12 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        // throw new \Exception('aa');
+        throw new \Exception('Yeah, that failed.');
     }
 
+    public function errorAction()
+    {
+        $this->response->resetHeaders();
+        $this->response->setStatusCode(500);
+    }
 }
