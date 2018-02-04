fd33948381006a6fc6ff7653754b53a5c5fcc227
Markus J Doetsch <git@the-enemy.org>
2018-02-04 19:42:13 +0100

    routing changes: module-specific routes

---
+++ app/modules/frontend/Routes.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Frontend;
+
+use Phalcon\Mvc\Router\Group;
+
+class Routes extends Group
+{
+    const INDEX_INDEX = 'frontend-index-home';
+
+    protected $hostname;
+
+    public function __construct($hostname = null)
+    {
+        $this->hostname = $hostname;
+        parent::__construct();
+    }
+
+    public function initialize()
+    {
+        $this->setPaths([
+            'module' => 'frontend',
+            'namespace' => 'Frontend\Controllers',
+        ]);
+
+        $this->setHostName($this->hostname);
+
+        // add routes below
+
+        // match the entry page '/'
+        $this->add('/', 'Index::index')->setName(self::INDEX_INDEX);
+    }
+}
