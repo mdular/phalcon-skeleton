6709fb0abb8320be9aafeac11a8fefbeac267626
Markus J Doetsch <git@the-enemy.org>
2018-03-25 22:30:37 +0200

    add csrf protection for contact form

--- app/modules/frontend/Form/ContactForm.php
+++ app/modules/frontend/Form/ContactForm.php
@@ -6,6 +6,7 @@ use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Submit;
 use Phalcon\Forms\Element\TextArea;
+use Phalcon\Forms\Element\Hidden;
 use Phalcon\Validation\Validator\Email;
 use Phalcon\Validation\Validator\PresenceOf;
 
@@ -14,6 +15,9 @@ class ContactForm extends Form
 {
     public function initialize ()
     {
+        $csrf = new Hidden($this->getCsrfTokenKey(), [
+            'value' => $this->getCsrfToken(),
+        ]);
 
         $email = new Text('email', [
             'placeholder' => 'Email (optional)',
@@ -34,8 +38,25 @@ class ContactForm extends Form
             'value' => 'Submit',
         ]);
 
+        $this->add($csrf);
         $this->add($email);
         $this->add($message);
         $this->add($submit);
+    }
+
+    public function getCsrfTokenKey ()
+    {
+        $tokenKeyId = '$PHALCON/CSRF/KEY$';
+
+        if ($this->session->has($tokenKeyId)) {
+            return $this->session->get($tokenKeyId);
+        }
+
+        return $this->security->getTokenKey();
+    }
+
+    public function getCsrfToken ()
+    {
+        return $this->security->getSessionToken() ? : $this->security->getToken();
     }
 }
