590ed5a06ed46d787171cac078a7d2a4f5f83ee4
Markus J Doetsch <git@the-enemy.org>
2018-05-08 09:27:38 +0200

    add react-router, not found default route, nav links, cleanup assets

--- client/admin/src/App.js
+++ client/admin/src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, NavLink, Link, Switch } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div id="adminapp">
+                    <header className="app_header">
+                        <h2 id="logo">
+                            <Link to="/">A Great Admin</Link>
+                        </h2>
+                        <nav>
+                            <NavLink to="/">Articles</NavLink>
+                            <NavLink to="/logout">Logout</NavLink>
+                        </nav>
+                    </header>
+
+                    <hr />
+
+                    <Switch>
+                        <Route render={() => <h1>Not found</h1>} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
